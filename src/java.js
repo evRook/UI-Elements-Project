6367a99e18f8963d5efbe2020024c9dd40109b40
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 19:24:10 -0700

    added images from API

--- src/java.js
+++ src/java.js
@@ -1,25 +1,14 @@
 console.log('Hello World')
 
-// fetch(`https://pokeapi.co/api/v2/pokemon/6`)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-
 let genOneImg = document.querySelectorAll('.gen1--img') //gets img containers
-let pkmnName = document.querySelector('.gen1--name')
 
 
-// for(i=1; i<genOneImg.length + 1; i++){
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(error => console.log('error'))
-// }
-
-for(i=1; i<genOneImg.length + 1; i++){
-    async function getPkmn(){
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+async function getPkmn(){
+    for(i=0; i<genOneImg.length; i++){
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`);
         let data = await response.json();
-        console.log(data.name)
+        console.log(data);
+        genOneImg[i].style.backgroundImage = `url('${data.sprites.front_default}')`;
     }
-    getPkmn();
 }
+getPkmn();
