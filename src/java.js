4c676b00b80c70d53d1061c84b81586e3087e087
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 19:58:59 -0700

    created a loopto make image containers & added modal HTML

--- src/java.js
+++ src/java.js
@@ -1,9 +1,16 @@
 console.log('Hello World')
 
-let genOneImg = document.querySelectorAll('.gen1--img') //gets img containers
+let genOne = document.querySelector('.js-gen1--container') //gets page container
 
+
+for(k=0; k < 151; k++){
+    let newDiv = document.createElement('div');
+    newDiv.className = 'gen1--img js-gen1--img';
+    genOne.appendChild(newDiv);
+}
 
 async function getPkmn(){
+    let genOneImg = document.querySelectorAll('.js-gen1--img') //gets img containers
     for(i=0; i<genOneImg.length; i++){
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`);
         let data = await response.json();
@@ -12,3 +19,4 @@ async function getPkmn(){
     }
 }
 getPkmn();
+
