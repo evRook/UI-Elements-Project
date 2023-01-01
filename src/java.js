a67514714de69bd50ad4dbc0d3b92190b792a337
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 18:13:18 -0700

    trying to learn fetch

--- src/java.js
+++ src/java.js
@@ -1,11 +1,15 @@
 console.log('Hello World')
 
-fetch(`https://pokeapi.co/api/v2/pokemon/6`)
-    .then(res => res.json())
+// fetch(`https://pokeapi.co/api/v2/pokemon/6`)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+
+let genOneImg = document.querySelectorAll('.gen1--img') //gets img containers
+
+for(i=1; i<genOneImg.length + 1; i++){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then(response => response.json())
     .then(response => console.log(response))
-
-let genOneImg = document.querySelectorAll('.gen1--img')
-
-for(i=0; i<genOneImg.length; i++){
-    genOneImg[i].innerText = `test${i}`
+    .catch(error => console.log('error'))
 }
+
