b873c51dde7f26e533e47302ec17b9bd5e0396ac
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 22:24:09 -0700

    woring on adding seceted pics to modal: incomplete

--- src/java.js
+++ src/java.js
@@ -10,11 +10,12 @@ for(k=0; k < 151; k++){
     genOne.appendChild(newDiv);
 }
 
-async function getPkmn(){
+async function getPkmn() {
     let genOneImg = document.querySelectorAll('.js-gen1--img') //gets img containers
-    for(i=0; i<genOneImg.length; i++){
+    for(let i=0; i<genOneImg.length; i++){
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`);
         let data = await response.json();
+        console.log(data)
         genOneImg[i].style.backgroundImage = `url('${data.sprites.front_default}')`;
     }
 }
@@ -22,11 +23,22 @@ getPkmn();
 
 let imgBtn = document.querySelectorAll('.js-gen1--img')
 
+async function modalContent(num) {
+    let normalSprite = document.getElementById('img1')
+    let shinySprite = document.getElementById('img2')
+    for(let m=0; m<imgBtn.length; m++){
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${m+1}`);
+        let data = await response.json();
+        normalSprite.style.backgroundImage = `url('${data.sprites.front_default}')`;
+        shinySprite.style.backgroundImage = `url('${data.sprites.front_shiny}')`;
+    }
+}
+
 for(let n=0; n<imgBtn.length; n++){
     imgBtn[n].addEventListener('click', openModal => {
         modalContainer.classList.add('js-visible--modal__container')
         modal.classList.add('js-visible--modal')
+        modalContent();
     })
-
 }
 
