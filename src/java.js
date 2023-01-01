07e4cfab653af7aacc9fbfc384df8c1dc8b6a944
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 21:44:17 -0700

    loop to selecteach modal individually

--- src/java.js
+++ src/java.js
@@ -1,7 +1,7 @@
 console.log('Hello World')
 
 let genOne = document.querySelector('.js-gen1--container') //gets page container
-let modal = document.querySelector('.js--modal')
+let modal = document.querySelector('.js-modal')
 let modalContainer = document.querySelector('.js-modal--container')
 
 for(k=0; k < 151; k++){
@@ -15,8 +15,18 @@ async function getPkmn(){
     for(i=0; i<genOneImg.length; i++){
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`);
         let data = await response.json();
-        console.log(data);
         genOneImg[i].style.backgroundImage = `url('${data.sprites.front_default}')`;
     }
 }
 getPkmn();
+
+let imgBtn = document.querySelectorAll('.js-gen1--img')
+
+for(let n=0; n<imgBtn.length; n++){
+    imgBtn[n].addEventListener('click', openModal => {
+        modalContainer.classList.add('js-visible--modal__container')
+        modal.classList.add('js-visible--modal')
+    })
+
+}
+
