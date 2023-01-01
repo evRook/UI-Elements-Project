31043c636b1d7d73bce4c9bdefb16756d384f151
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 18:26:48 -0700

    trying to learn async function with fetch

--- src/java.js
+++ src/java.js
@@ -5,11 +5,21 @@ console.log('Hello World')
 //     .then(response => console.log(response))
 
 let genOneImg = document.querySelectorAll('.gen1--img') //gets img containers
+let pkmnName = document.querySelector('.gen1--name')
+
+
+// for(i=1; i<genOneImg.length + 1; i++){
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(error => console.log('error'))
+// }
 
 for(i=1; i<genOneImg.length + 1; i++){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(error => console.log('error'))
+    async function getPkmn(){
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        let data = await response.json();
+        console.log(data.name)
+    }
+    getPkmn();
 }
-
