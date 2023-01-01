7ea38ae3bf50a4eaab76f337e8d36344fd865d8a
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-31 17:19:03 -0700

    testing loop

--- src/java.js
+++ src/java.js
@@ -1,5 +1,11 @@
 console.log('Hello World')
 
-fetch("https://pokeapi.co/api/v2/pokemon/6")
-.then(res => res.json())
-.then(response => console.log(response))
+fetch(`https://pokeapi.co/api/v2/pokemon/6`)
+    .then(res => res.json())
+    .then(response => console.log(response))
+
+let genOneImg = document.querySelectorAll('.gen1--img')
+
+for(i=0; i<genOneImg.length; i++){
+    genOneImg[i].innerText = `test${i}`
+}
